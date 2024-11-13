cb9ef633fc1e3161a339b6da3fc9699e8bacd8c7
sumeyye <sumeyye@sumeyye-Yoga-Slim>
2024-11-13 13:43:30 +0300

    services updated

--- backend/src/main/java/com/example/backend/service/DataLoaderService.java
+++ backend/src/main/java/com/example/backend/service/DataLoaderService.java
@@ -1,29 +1,29 @@
-// package com.example.backend.service;
+package com.example.backend.service;
 
-// import org.springframework.beans.factory.annotation.Autowired;
-// import org.springframework.stereotype.Service;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
-// import jakarta.annotation.PostConstruct;
-// import javax.json.Json;
-// import javax.json.JsonObject;
-// import javax.json.JsonReader;
-// import java.io.InputStream;
+import jakarta.annotation.PostConstruct;
+import javax.json.Json;
+import javax.json.JsonObject;
+import javax.json.JsonReader;
+import java.io.InputStream;
 
-// @Service
-// public class DataLoaderService {
+@Service
+public class DataLoaderService {
 
-//     @Autowired
-//     private TransactionalDataLoaderService transactionalDataLoaderService;
+    @Autowired
+    private TransactionalDataLoaderService transactionalDataLoaderService;
 
-//     @PostConstruct
-//     public void loadData() {
-//         try (InputStream is = getClass().getResourceAsStream("/config.json")) {
-//             JsonReader reader = Json.createReader(is);
-//             JsonObject jsonObject = reader.readObject();
+    @PostConstruct
+    public void loadData() {
+        try (InputStream is = getClass().getResourceAsStream("/config.json")) {
+            JsonReader reader = Json.createReader(is);
+            JsonObject jsonObject = reader.readObject();
 
-//             transactionalDataLoaderService.load(jsonObject.getJsonArray("tableDetails"), jsonObject.getString("databaseUser"));
-//         } catch (Exception e) {
-//             e.printStackTrace();
-//         }
-//     }
-// }
+            transactionalDataLoaderService.load(jsonObject.getJsonArray("tableDetails"), jsonObject.getString("databaseUser"));
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+}
